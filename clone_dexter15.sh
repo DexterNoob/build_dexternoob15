f4f3e726e7755d281a5b99d20c539803ef16cb9a
dexter <115158271+DexterNoob@users.noreply.github.com>
2022-11-25 05:39:51 +0700

    Update clone_dexter15.sh

--- clone_dexter15.sh
+++ clone_dexter15.sh
@@ -3,6 +3,6 @@
 dexter="$(pwd)/dexter"
 
 if [ ! -d $dexter ]; then
-    wget "$(curl -s https://github.com/noobbeast/dexter-clang/releases/download/DexterNoob-Clang-15.0.6-20221124-release/DexterNoob-Clang-15.0.6-20221124.tar)" -O "DexterNoob-Clang-15.0.6.tar.gz"
+    wget "$(curl -s https://raw.githubusercontent.com/noobbeast/dexter-clang/main/15.0.6/link.txt)" -O "DexterNoob-Clang-15.0.6.tar.gz"
     mkdir dexter && tar -xf DexterNoob-Clang-15.0.6.tar.gz -C dexter && rm -rf DexterNoob-Clang-15.0.6.tar.gz link.txt 
 fi
