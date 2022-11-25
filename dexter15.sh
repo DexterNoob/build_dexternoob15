c41672b2d7a8bc436952f86995f970812a785210
dexter <115158271+DexterNoob@users.noreply.github.com>
2022-11-25 16:25:10 +0700

    Update dexter15.sh

--- dexter15.sh
+++ dexter15.sh
@@ -35,7 +35,7 @@ ZIP_KERNEL_VERSION="4.14.$(cat "$MainPath/Makefile" | grep "SUBLEVEL =" | sed 's
 TIME=$(date +"%m%d%H%M")
 
 # Start building
-Compiler=Begonia-Thin15
+Compiler=clang-r15
 MAKE="./makeparallel"
 rm -rf out
 BUILD_START=$(date +"%s")
